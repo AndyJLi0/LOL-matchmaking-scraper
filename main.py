7155fca919e513f4f992ae97c0a4f1b1fc95c649
Paingle <andyjiaanli@gmail.com>
2023-08-19 03:27:48 -0700

    formatted table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,28 +11,50 @@ def main(name):
     driver = webdriver.Chrome(PATH)
     driver.get(f'https://www.op.gg/summoners/na/{name}/ingame')
     driver.implicitly_wait(5)
-    # Locate the table
-    table = driver.find_element(By.XPATH, '//*[@id="content-container"]/div/table[1]/tbody')
 
-    # Get all rows of the table
-    rows = table.find_elements(By.TAG_NAME, "tr")
+    blue_side_info = []
+    red_side_info = []
+    try:
+        blue_table = driver.find_element(By.XPATH, '//*[@id="content-container"]/div/table[1]/tbody')
+        blue_rows = blue_table.find_elements(By.TAG_NAME, "tr")
 
-    # Iterate through rows to extract summoner names
-    summoner_names = []
-    for row in rows:
-        # Get columns for the current row
-        columns = row.find_elements(By.TAG_NAME, "td")
+        red_table = driver.find_element(By.XPATH, '//*[@id="content-container"]/div/table[2]/tbody')
+        red_rows = red_table.find_elements(By.TAG_NAME, "tr")
 
-        # Assuming summoner name is in the first column (adjust index as needed)
-        summoner_name = columns[3].text
-        summoner_rank = columns[5].text
-        summoner_names.append(summoner_name)
-        summoner_names.append(summoner_rank)
+        for row in blue_rows:
+            blue_columns = row.find_elements(By.TAG_NAME, "td")
 
-    formatted_data = [names.replace('\n', ', ') for names in summoner_names]
-    for i in range(0, len(formatted_data), 2):
-        print(f'| {formatted_data[i]} --> {formatted_data[i+1]} |')
-        print("----------------------------------------------")
+            summoner_name = blue_columns[3].text
+            summoner_rank = blue_columns[5].text
+            blue_side_info.append(summoner_name)
+            blue_side_info.append(summoner_rank)
 
+        for row in red_rows:
+            red_columns = row.find_elements(By.TAG_NAME, "td")
 
-main("nerner")
+            summoner_name = red_columns[3].text
+            summoner_rank = red_columns[5].text
+            red_side_info.append(summoner_name)
+            red_side_info.append(summoner_rank)
+
+        blue_formatted_data = [names.replace('\n', ', ') for names in blue_side_info]
+        red_formatted_data = [names.replace('\n', ', ') for names in red_side_info]
+
+        # Format and print table
+        title = "GAME INFO"
+        title = title.rjust(59)
+        print(f'{title}')
+        print('-' * 108)
+        for i in range(0, len(blue_formatted_data), 2):
+            left = blue_formatted_data[i] + " " + blue_formatted_data[i + 1]
+            left = left.ljust(50)
+            right = red_formatted_data[i] + " " + red_formatted_data[i + 1]
+            right = right.rjust(50)
+            print(f"| {left} || {right} |")
+            print('-' * 108)
+
+    except NoSuchElementException:
+        print("ERROR: the user is not currently in a game!")
+        driver.close()
+
+main("tyrant003")
